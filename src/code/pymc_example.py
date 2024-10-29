1a073cf660bee9b3db340a4d47f427d02eb177b2
Matthew Feickert <matthew.feickert@cern.ch>
2024-10-29 01:28:30 -0600

    Get citations

--- src/code/pymc_example.py
+++
@@ -1,4 +0,0 @@
-with infer.model(model, unconstr_priors, data):
-    post_data = pymc.sample(draws=10_000, chains=1)
-    post_pred = pymc.sample_posterior_predictive(post_data)
-    prior_pred = pymc.sample_prior_predictive(10_000)
